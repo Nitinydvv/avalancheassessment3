a5915a795e8777c907d3873c953fb5e4f24221ee
Nitinydvv <nitinyadav66829@gmail.com>
2024-09-08 18:05:48 +0530

    Update myToken.sol

--- myToken.sol
+++ myToken.sol
@@ -8,12 +8,19 @@ contract MyToken is ERC20 {
 
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
         owner = msg.sender; // Contract deployer is set as the owner
-        _mint(msg.sender, 10000 * 10 ** decimals()); // Mint initial supply to the owner
+        _mint(msg.sender, 10000); // Mint initial supply to the owner
     }
 
     function mint(address account, uint256 amount) public {
         require(msg.sender == owner, "Only the owner can mint tokens");
         _mint(account, amount);
+    }
+
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+
+        require(amount > 0, "Transfer amount must be greater than zero");
+
+        return super.transfer(recipient, amount);
     }
 
     function burn(uint256 amount) public {
